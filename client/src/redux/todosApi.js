255395ccab274553e7c491e785c2047cf385f368
Sambuka <vova.kurtiak@gmail.com>
2022-12-23 21:51:55 +0100

    client fix

--- client/src/redux/todosApi.js
+++ client/src/redux/todosApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const todosApi = createApi({
     reducerPath: 'todosApi',
     tagTypes: ['Todos'],
-    baseQuery: fetchBaseQuery({baseUrl: 'https://fe-mern-todo-app.onrender.com'}),
+    baseQuery: fetchBaseQuery({baseUrl: 'https://fe-mern-todo-app-api.onrender.com'}),
     endpoints: (build) => ({
         getTodos: build.query({
             query: () => `todos` ,
